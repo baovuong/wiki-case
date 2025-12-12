1cb0d3b201de80cdbfe2f4728a5bfd4a587922f6
Bao Vuong <vqbao@lv5.org>
2025-12-12 02:27:53 -0500

    able to read gresource

--- src/main.c
+++ src/main.c
@@ -3,11 +3,12 @@
 #include "glib.h"
 #include <gtk/gtk.h>
 
+#include "gresource.h"
+
 static void print_hello(GtkWidget *widget, gpointer data)
 {
     g_print("Hello, World\n");
 }
-
 
 int main(int argc, char **argv)
 {
@@ -15,6 +16,8 @@ int main(int argc, char **argv)
     GtkBuilder *builder;
     GObject *window;
     GObject *button;
+    GtkTextView *textView;
+    GtkTextBuffer *buffer;
     GResource *resource;
     GBytes *builder_data;
     GError *error = NULL;
@@ -24,18 +27,19 @@ int main(int argc, char **argv)
     g_print("wow\n");
     resource = wiki_case_get_resource();
 
-    g_print("loaded\n");
+
+    g_print("resource loaded\n");
     builder = gtk_builder_new();
 
     // builder data
-    builder_data = g_resource_lookup_data(resource, "/org/gtk/Example/builder.ui", G_RESOURCE_LOOKUP_FLAGS_NONE, &error);
+    builder_data = g_resource_lookup_data(resource, "/org/gtk/Example/res/builder.ui", G_RESOURCE_LOOKUP_FLAGS_NONE, &error);
 
-    g_print("loaded\n");
+    g_print("builder loaded\n");
 
     if (gtk_builder_add_from_string(builder, 
         g_bytes_get_data(builder_data, NULL), 
         g_bytes_get_size(builder_data), 
-        &error) != 0)
+        &error) == 0)
     {
         g_printerr("Error loading data: %s\n", error->message);
         g_clear_error(&error);
@@ -50,12 +54,10 @@ int main(int argc, char **argv)
     button = gtk_builder_get_object(builder, "button1");
     g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
 
-    button = gtk_builder_get_object(builder, "button2");
-    g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
+    textView = GTK_TEXT_VIEW(gtk_builder_get_object(builder, "contentTextView"));
+    buffer = gtk_text_view_get_buffer(textView);
 
-    button = gtk_builder_get_object(builder, "quit");
-    g_signal_connect(button, "clicked", G_CALLBACK(gtk_main_quit), NULL);
-
+    gtk_text_buffer_set_text(buffer, "LMAO", 4);
     gtk_main();
 
     return 0;
